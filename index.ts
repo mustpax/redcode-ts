57240ed6531813af5dfbdedf2ee548e8a80becb7
Mustafa "Moose" Paksoy <paksoy@gmail.com>
2025-03-04 13:36:22 -0800

    wip

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-console.log("Hello via Bun!");
+import { createToken, Lexer } from "chevrotain";
+
+export const selectLexer = new Lexer([
+  createToken({
+    name: "Newline",
+    pattern: /\r\n|\n/,
+  }),
+  createToken({
+    name: "Whitespace",
+    pattern: /( |\t)+/,
+    group: Lexer.SKIPPED,
+  }),
+  createToken({
+    name: "Identifier",
+    pattern: /[a-zA-Z]\w*/,
+  }),
+]);
+
+export function lex(input: string) {
+  const result = selectLexer.tokenize(input);
+
+  if (result.errors.length > 0) {
+    throw new Error(result.errors[0].message);
+  }
+
+  return result.tokens;
+}
+
+console.log(
+  lex(`
+  `)
+);
