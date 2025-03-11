3d659cde1740a742ded8c1c0e12e26effde6377d
Mustafa "Moose" Paksoy <paksoy@gmail.com>
2025-03-11 11:53:24 -0700

    wip

--- parse_test.ts
+++ parse_test.ts
@@ -1,5 +1,15 @@
-import { test, expect } from "bun:test";
+import { test, expect, describe } from "bun:test";
+import { parse } from "./gen/redcode.js";
+import { readdir, readFile } from "node:fs/promises";
 
-test("snap", () => {
-  expect("foo").toMatchSnapshot();
+describe("parse all examples", async () => {
+  // read all the files in the current directory
+  const files = await readdir("redcode_examples");
+  for (const file of files) {
+    test(file, async () => {
+      const content = await readFile(`redcode_examples/${file}`, "utf8");
+      const result = parse(content);
+      expect(result).toMatchSnapshot();
+    });
+  }
 });
