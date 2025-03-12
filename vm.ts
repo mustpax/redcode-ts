b4db967d70881e34f2a717e62818409c49e08543
Mustafa "Moose" Paksoy <paksoy@gmail.com>
2025-03-12 12:52:35 -0700

    wip

--- vm.ts
+++ vm.ts
@@ -6,14 +6,24 @@ import {
 } from "./redcode";
 
 export function runProgram(program: RedcodeProgram) {
-  const memory = program.map((line) => line.instruction).filter((i) => i);
+  const memorySize = 40;
+  const programInstr = program.map((line) => line.instruction).filter((i) => i);
+  const memory = new Array<Instruction>(memorySize)
+    .fill({
+      opcode: "DAT",
+      args: null,
+      modifier: null,
+    })
+    .map((item, index) => programInstr[index] ?? item);
   let pc = 0;
 
-  while (pc < program.length) {
+  while (pc < memory.length) {
     const instruction = memory[pc];
-    if (instruction) {
-      console.log(instructionToString(instruction));
+    console.log(instructionToString(instruction));
+    if (instruction.opcode === "DAT") {
+      break;
     }
     pc++;
   }
+  console.log("stopped at address", pc, instructionToString(memory[pc]));
 }
